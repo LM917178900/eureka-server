55f4ca46c5fa8d34547283d9351c99c3e9f9c14b
leiming5 <leiming5@lenovo.com>
2020-06-08 20:16:04 +0800

    first update

---
+++ src/main/java/com/huahua/demo/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.huahua.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 能够被发现为服务端
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
